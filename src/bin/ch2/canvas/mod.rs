f858caecfdcec7fcd7fcc090c9a6bdd652e5b576
Daniel Bearden <danbear0@gmail.com>
2021-04-14 19:46:51 -0700

    Chapter 2

---
+++ src/bin/ch2/canvas/mod.rs
@@ -0,0 +1,70 @@
+use std::{
+    fs::File,
+    io::{self, BufWriter},
+};
+
+use io::Write;
+
+use super::tuple::Color;
+pub struct Canvas {
+    pub width: usize,
+    pub height: usize,
+    pub pixels: Vec<Color>,
+}
+
+impl Canvas {
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            width,
+            height,
+            pixels: vec![Color::default(); width * height],
+        }
+    }
+
+    pub fn write(&mut self, x: usize, y: usize, color: Color) {
+        self.pixels[(self.width * y) + x] = color;
+    }
+
+    pub fn pixel_at(&self, x: usize, y: usize) -> Color {
+        self.pixels[(self.width * y) + x]
+    }
+
+    pub fn to_ppm(&self, filename: &str) -> io::Result<()> {
+        let f = File::create(filename)?;
+        let mut w = BufWriter::new(f);
+        let header = format!("P3\n{} {}\n255", self.width, self.height);
+        w.write(header.as_bytes())?;
+        for (i, pixel) in self.pixels.iter().enumerate() {
+            if i % self.width == 0 {
+                w.write(b"\n")?;
+            }
+            w.write((*pixel * 255f64).to_string().as_bytes())?;
+            w.write(b" ")?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn canvas_creation() {
+        let c = Canvas::new(10, 20);
+        assert_eq!(c.width, 10);
+        assert_eq!(c.height, 20);
+        assert!(c.pixels.iter().all(|&c| c == Color::default()));
+    }
+
+    #[test]
+    fn write_pixel() {
+        let mut c = Canvas::new(10, 20);
+        let red = Color::new(1.0, 0.0, 0.0);
+        let x = 2;
+        let y = 3;
+        c.write(x, y, red);
+        assert_eq!(c.pixel_at(x, y), red);
+    }
+}
--- src/bin/ch2/main.rs
+++ src/bin/ch2/main.rs
@@ -1,5 +1,10 @@
 mod tuple;
-use tuple::{Point, Vector};
+use std::{fmt::Display, fs::File, io::Write};
+
+use tuple::{Color, Point, Tuple, Vector};
+mod canvas;
+use canvas::Canvas;
+
 struct Projectile {
     position: Point,
     velocity: Vector,
@@ -8,11 +13,10 @@ struct Environment {
     gravity: Vector,
     wind: Vector,
 }
-
 fn main() {
     let mut p = Projectile {
         position: Point::new(0.0, 1.0, 0.0),
-        velocity: Vector::new(1.0, 1.0, 0.0).normalize(),
+        velocity: Vector::new(1.0, 2.0, 0.0).normalize() * 10.0,
     };
 
     let e = Environment {
@@ -20,10 +24,31 @@ fn main() {
         wind: Vector::new(-0.01, 0.0, 0.0),
     };
 
-    while p.position.y > 0f64 {
+    let mut path = Vec::new();
+
+    let mut max_x = 0f64;
+    let mut max_y = 0f64;
+    while p.position.y >= 0f64 {
         println!("x: {}, y: {}", p.position.x, p.position.y);
+        path.push((p.position.x, p.position.y));
         p = tick(&e, p);
+        max_x = max_x.max(p.position.x);
+
+        max_y = max_y.max(p.position.y);
     }
+    path.push((p.position.x, p.position.y));
+
+    let mut c = Canvas::new(max_x as usize + 1, max_y as usize + 1);
+
+    for (x, y) in path {
+        c.write(
+            x as usize,
+            c.height - y as usize - 1,
+            Color::new(x / c.width as f64, y / c.height as f64, 1.0),
+        );
+    }
+
+    c.to_ppm("foo.ppm").unwrap();
 }
 
 fn tick(env: &Environment, proj: Projectile) -> Projectile {
